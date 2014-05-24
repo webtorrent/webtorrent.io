29f8e570ea43e5153df79213f10e6bf39873d2ac
Feross Aboukhadijeh <feross@feross.org>
2014-05-23 23:24:08 -0700

    don't use variable in shell script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 # Update code and restart server (run from app server)
 
-APP_DIR = /home/feross/www/webtorrent.io
-
-cd $(APP_DIR) && git pull
-cd $(APP_DIR) && npm update --quiet
+cd /home/feross/www/webtorrent.io && git pull
+cd /home/feross/www/webtorrent.io && npm update --quiet
 # cd $(APP_DIR) && npm run build
 sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart webtorrent.io
--- bin/trigger.sh
+++ bin/trigger.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # Trigger a deploy (run from CI server)
 
-APP_DIR = /home/feross/www/webtorrent.io
-
-ssh feross@future.feross.net -p 44444 $(APP_DIR)/bin/deploy.sh
+ssh feross@future.feross.net -p 44444 /home/feross/www/webtorrent.io/bin/deploy.sh
