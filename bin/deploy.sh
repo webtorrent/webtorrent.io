e408a0e9e8db298e37d968b1cd2cd376226f8143
Feross Aboukhadijeh <feross@feross.org>
2014-07-04 00:01:25 -0700

    use new linode

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@
 cd /home/feross/www/webtorrent.io && git pull
 cd /home/feross/www/webtorrent.io && npm update --quiet
 # cd $(APP_DIR) && npm run build
-sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart webtorrent.io
+sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart webtorrent
