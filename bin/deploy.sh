e21874d72b9566a78898b8a81b35061d8fcc413b
Feross Aboukhadijeh <feross@feross.org>
2014-07-05 14:48:48 -0700

    fix paths

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Update code and restart server (run from app server)
 
-cd /home/feross/www/webtorrent.io && git pull
-cd /home/feross/www/webtorrent.io && npm update --quiet
+cd /home/feross/www/webtorrent-website && git pull
+cd /home/feross/www/webtorrent-website && npm update --quiet
 # cd $(APP_DIR) && npm run build
 sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart webtorrent
--- bin/trigger.sh
+++ bin/trigger.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Trigger a deploy (run from CI server)
 
-ssh feross@webtorrent.feross.net -p 44444 /home/feross/www/webtorrent.io/bin/deploy.sh
+ssh feross@webtorrent.feross.net -p 44444 /home/feross/www/webtorrent-website/bin/deploy.sh
