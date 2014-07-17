96c9491b0887d437e26869bc6db19c18f1667b6e
Feross Aboukhadijeh <feross@feross.org>
2014-07-17 00:52:44 -0700

    fail deploy script on error

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Update code and restart server (run from app server)
-
+set -e
 cd /home/feross/www/webtorrent.io && git pull
 cd /home/feross/www/webtorrent.io && npm update --quiet
 cd /home/feross/www/webtorrent.io && npm run build
