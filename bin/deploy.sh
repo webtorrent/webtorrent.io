ece05ca9a060dcd3dc0bbaa936c89283d35e715b
Feross Aboukhadijeh <feross@feross.org>
2014-07-17 00:29:52 -0700

    fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@
 
 cd /home/feross/www/webtorrent.io && git pull
 cd /home/feross/www/webtorrent.io && npm update --quiet
-cd $(APP_DIR) && npm run build
+cd /home/feross/www/webtorrent.io && npm run build
 sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart webtorrent
