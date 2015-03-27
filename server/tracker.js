0674683ff4bf88c34e3ed602769c14cf9bf82935
Feross Aboukhadijeh <feross@feross.org>
2015-03-27 16:19:57 +1300

    use integrated bittorrent/webtorrent tracker

--- server/tracker.js
+++ server/tracker.js
@@ -2,24 +2,22 @@ var config = require('../config')
 var debug = require('debug')('webtorrent-website:tracker')
 var util = require('../util')
 
-var BitTorrentTracker = require('bittorrent-tracker/server')
-var WebTorrentTracker = require('webtorrent-tracker/server')
+var Tracker = require('bittorrent-tracker/server')
 
 util.upgradeLimits()
 
-var btTracker = new BitTorrentTracker()
+var tracker = new Tracker({
+  ws: true // enable websocket (webtorrent) tracker
+})
 
-// Add a websocket server at the same URL
-var wtTracker = new WebTorrentTracker({ server: btTracker.http })
-
-btTracker.on('listening', function () {
+tracker.on('listening', function () {
   var ports = {
-    http: btTracker.http.address().port,
-    udp: btTracker.udp.address().port
+    http: tracker.http.address().port,
+    udp: tracker.udp.address().port
   }
   debug('listening on ' + JSON.stringify(ports))
   util.downgradeUid()
   process.send('ready')
 })
 
-btTracker.listen(config.ports.tracker)
+tracker.listen(config.ports.tracker)
