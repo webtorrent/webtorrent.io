301476ab81dba620eee9871454956ca03ecb985a
Feross Aboukhadijeh <feross@feross.org>
2015-03-27 16:34:09 +1300

    force re-install modules

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,7 @@
 # Update code and restart server (run from app server)
 set -e
 cd /home/feross/www/webtorrent.io && git pull
-cd /home/feross/www/webtorrent.io && npm update --quiet
+cd /home/feross/www/webtorrent.io && rm -rf node_modules
+cd /home/feross/www/webtorrent.io && npm install --quiet
 cd /home/feross/www/webtorrent.io && npm run build
 sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart webtorrent
