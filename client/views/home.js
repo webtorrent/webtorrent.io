3949c1102594e4a2949052d0e913dadc0b5d62cc
Feross Aboukhadijeh <feross@feross.org>
2015-07-29 05:10:30 -0700

    fix standard

--- client/views/home.js
+++ client/views/home.js
@@ -1,27 +1,24 @@
+/* global $ */
 module.exports = function () {
-  var pre_l
-  var content_l
-  var fp_l
-  var intro_l
-  var s2_l
-
   $(document).ready(function () {
 
-    pre_l = setTimeout(function () {
+    setTimeout(function () {
       $('#pre').fadeOut(600)
     }, 300)
 
-    content_l = setTimeout(function () {
+    setTimeout(function () {
       $('#particles').fadeIn(400)
     }, 1500)
 
-    fp_l = setTimeout(function () {
+    setTimeout(function () {
       $('#fp-nav').fadeIn(300)
     }, 1500)
-    intro_l = setTimeout(function () {
+
+    setTimeout(function () {
       $('#s1').fadeIn(600)
     }, 2000)
-    s2_l = setTimeout(function () {
+
+    setTimeout(function () {
       $('#s2').fadeIn(400)
     }, 1500)
 
