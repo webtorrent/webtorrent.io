ea0b4a444da70b4a780d72be3efdb8d9cf85dd95
Feross Aboukhadijeh <feross@feross.org>
2015-11-21 19:02:19 -0800

    Near zero-downtime deploy (fix #17)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,9 +2,23 @@
 # Update code and restart server (run from app server)
 trap 'exit' ERR
 
+if [ -d "/home/feross/www/webtorrent.io-build" ]; then
+  echo "ERROR: Build folder already exists. Is another build in progress?"
+  exit 1
+fi
+
+cp -R /home/feross/www/webtorrent.io /home/feross/www/webtorrent.io-build
+
+cd /home/feross/www/webtorrent.io-build && git pull
+cd /home/feross/www/webtorrent.io-build && rm -rf node_modules
+cd /home/feross/www/webtorrent.io-build && npm install --quiet
+cd /home/feross/www/webtorrent.io-build && npm run build
+
 sudo supervisorctl stop webtorrent
-cd /home/feross/www/webtorrent.io && git pull
-cd /home/feross/www/webtorrent.io && rm -rf node_modules
-cd /home/feross/www/webtorrent.io && npm install --quiet
-cd /home/feross/www/webtorrent.io && npm run build
+
+cd /home/feross/www && mv webtorrent.io webtorrent.io-old
+cd /home/feross/www && mv webtorrent.io-build webtorrent.io
+
 sudo supervisorctl start webtorrent
+
+cd /home/feross/www && rm -rf webtorrent.io-old
