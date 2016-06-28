1a8ecf907e37b0ceaf8bf925512edcd3d6f96645
DC <dcposch@dcpos.ch>
2016-06-28 07:00:11 -0700

    GET /desktop/telemetry with basic auth

--- server/tracker.js
+++ server/tracker.js
@@ -1,4 +1,4 @@
-var auth = require('basic-auth')
+var auth = require('./auth')
 var config = require('../config')
 var debug = require('debug')('webtorrent-www:tracker')
 var downgrade = require('downgrade')
@@ -10,41 +10,37 @@ unlimited()
 
 // Create WebTorrent-only tracker. Disable UDP and HTTP.
 var tracker = new Tracker({ ws: true, udp: false, http: false })
+var basicAuth = auth(secret.credentials)
 
 // Redirect https://tracker.webtorrent.io to website homepage
-tracker.http.on('request', function (req, res, opts) {
+tracker.http.on('request', function (req, res) {
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(301, { 'Location': 'https://webtorrent.io' })
     res.end()
   } else if (req.method === 'GET' && req.url === '/debug') {
-    var credentials = auth(req)
-    if (!credentials ||
-      credentials.user !== secret.debugCredentials.user ||
-      credentials.pass !== secret.debugCredentials.pass) {
-      res.statusCode = 401
-      res.setHeader('WWW-Authenticate', 'Basic realm="debug"')
-      res.end('404 Not Found')
-    }
-
-    var infoHashes = Object.keys(tracker.torrents)
-
-    var list = []
-    infoHashes.forEach(function (infoHash) {
-      var peerIds = Object.keys(tracker.torrents[infoHash].peers)
-      var numPeers = 0
-      peerIds.forEach(function (peerId) {
-        if (tracker.torrents[infoHash].peers[peerId] !== null) numPeers += 1
-      })
-      if (numPeers === 0) return
-      list.push('<a target="_blank" href="https://instant.io/#' + infoHash + '">' + infoHash + '</a>')
-    })
-
-    var html = '<h1>' + infoHashes.length + ' torrents (' + list.length + ' active)</h1>\n'
-    html += list.join('\n<br>')
-
-    res.end(html)
+    basicAuth(req, res, () => getDebugHtml(req, res))
   }
 })
+
+function getDebugHtml (req, res) {
+  var infoHashes = Object.keys(tracker.torrents)
+
+  var list = []
+  infoHashes.forEach(function (infoHash) {
+    var peerIds = Object.keys(tracker.torrents[infoHash].peers)
+    var numPeers = 0
+    peerIds.forEach(function (peerId) {
+      if (tracker.torrents[infoHash].peers[peerId] !== null) numPeers += 1
+    })
+    if (numPeers === 0) return
+    list.push('<a target="_blank" href="https://instant.io/#' + infoHash + '">' + infoHash + '</a>')
+  })
+
+  var html = '<h1>' + infoHashes.length + ' torrents (' + list.length + ' active)</h1>\n'
+  html += list.join('\n<br>')
+
+  res.end(html)
+}
 
 tracker.on('listening', function () {
   debug('listening on %s', tracker.ws.address().port)
