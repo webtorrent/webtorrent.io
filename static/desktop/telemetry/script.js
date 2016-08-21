1e991f3d5fe2035cbf31e25ee492ee8da936bd50
DC <dcposch@dcpos.ch>
2016-08-20 20:42:40 -0700

    Telemetry dashboard: add explanations

--- static/desktop/telemetry/script.js
+++ static/desktop/telemetry/script.js
@@ -40,10 +40,8 @@ var dateFormat = function (t) {
 window.addEventListener('DOMContentLoaded', function () {
   console.log('Graphing...')
 
-  var charts = window.charts = chartInfos.map(function (info, i) {
-    var chart = i === 1
-      ? nv.models.lineWithFocusChart()
-      : nv.models.lineChart()
+  window.charts = chartInfos.map(function (info, i) {
+    var chart = nv.models.lineWithFocusChart()
 
     chart.xAxis
       .scale(dateScale)
@@ -61,80 +59,79 @@ window.addEventListener('DOMContentLoaded', function () {
       .transition().duration(500)
       .call(chart)
 
+    updateEvents(chart, i)
+    chart.focus.dispatch.on('brush', function (e) {
+      setTimeout(() => updateEvents(chart, i), 0)
+    })
+
     return chart
   })
 
-  updateEvents()
-
   nv.utils.windowResize(updateAll)
-
-  charts[1].focus.dispatch.on('brush', function (e) {
-    charts[0].xDomain(e.extent).update()
-    setTimeout(updateEvents, 0)
-  })
 })
 
 function updateAll () {
   window.charts.forEach(function (chart, i) {
     chart.update()
+    updateEvents(chart, i)
   })
-  updateEvents()
 }
 
-function updateEvents () {
-  window.charts.forEach(function (chart, i) {
-    // Then, draw a red line at each event (eg, new release of the app)
-    var xDomain = chart.xAxis.domain()
-    var yDomain = chart.yAxis.domain()
-    var events = summary.releases.map(function (release) {
-      return {
-        t: new Date(release.published_at).getTime(),
-        name: release.tag_name
-      }
-    })
-    events = events.filter(function (release) {
-      return release.t > xDomain[0] && release.t < xDomain[1]
-    })
-    var scale = function (event) {
-      return chart.xAxis.scale()(event.t)
+// Draw a red line at each event (eg, new release of the app)
+function updateEvents (chart, i) {
+  var xDomain = chart.xAxis.domain()
+  var yDomain = chart.yAxis.domain()
+  var events = summary.releases.map(function (release) {
+    return {
+      t: new Date(release.published_at).getTime(),
+      name: release.tag_name
     }
-
-    // First, draw the lines
-    var container = d3.select(chartInfos[i].selector + ' .nv-interactive')
-    var yPixels = yDomain.map(chart.yAxis.scale())
-    var sel = container
-      .selectAll('line.event')
-      .data(events)
-      .attr('x1', scale)
-      .attr('x2', scale)
-      .attr('y1', yPixels[0])
-      .attr('y2', yPixels[1])
-    sel.enter()
-      .append('line')
-      .attr('class', 'event')
-      .attr('x1', scale)
-      .attr('x2', scale)
-      .attr('y1', yPixels[0])
-      .attr('y2', yPixels[1])
-      .style('stroke', '#faa')
-    sel.exit()
-      .remove()
-
-    // Then, draw labels
-    var sel = container
-      .selectAll('text.event')
-      .data(events)
-      .attr('x', scale)
-      .attr('y', 10)
-      .text(function (event) { return event.name })
-    sel.enter()
-      .append('text')
-      .attr('class', 'event')
-      .attr('x', scale)
-      .attr('y', 10)
-      .style('fill', '#f88')
-      .text(function (event) { return event.name })
-    sel.exit()
-      .remove()
   })
+  events = events.filter(function (release) {
+    return release.t > xDomain[0] && release.t < xDomain[1]
+  })
+
+  // First, draw the lines
+  var yPixels = yDomain.map(chart.yAxis.scale())
+  var xScale = function (event) {
+    return chart.xAxis.scale()(event.t)
+  }
+
+  var container = d3.select(chartInfos[i].selector + ' .nv-interactive')
+
+  var sel = container
+    .selectAll('line.event')
+    .data(events)
+
+  sel.enter()
+    .append('line')
+    .attr('class', 'event')
+
+  sel.attr('x1', xScale)
+    .attr('x2', xScale)
+    .attr('y1', yPixels[0])
+    .attr('y2', yPixels[1])
+
+  sel.exit()
+    .remove()
+
+  // Then, draw labels
+  var textXScale = function (event) {
+    return xScale(event) + 2
+  }
+
+  sel = container
+    .selectAll('text.event')
+    .data(events)
+
+  sel.enter()
+    .append('text')
+    .attr('class', 'event')
+
+  sel.attr('x', textXScale)
+    .attr('y', 12)
+    .text(function (event) { return event.name })
+
+  sel.exit()
+    .remove()
 }
