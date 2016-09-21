9df112efe257a064bf300fdcc8f23c28c7ee4356
Feross Aboukhadijeh <feross@feross.org>
2016-09-21 12:33:46 +0200

    Turn off tracker.webtorrent.io (#106)

--- server/index.js
+++ server/index.js
@@ -40,33 +40,21 @@ try {
 } catch (err) {}
 
 function onRequest (req, res) {
-  if (req.headers.host === 'tracker.webtorrent.io' ||
-      req.headers.host === 'tracker.webtorrent.io:' + config.ports.router.https) {
-    proxy.web(req, res, { target: 'http://127.0.0.1:' + config.ports.tracker })
-  } else if (req.headers.host === 'whiteboard.webtorrent.io' ||
-      req.headers.host === 'whiteboard.webtorrent.io:' + config.ports.router.https) {
-    proxy.web(req, res, { target: 'http://127.0.0.1:' + config.ports.whiteboard })
-  } else if (req.headers.host === 'peerdb.io') {
+  if (req.headers.host === 'peerdb.io') {
     proxy.web(req, res, { target: 'http://127.0.0.1:' + config.ports.peerdb })
   } else {
     proxy.web(req, res, { target: 'http://127.0.0.1:' + config.ports.web })
   }
 }
 
-function onUpgrade (req, socket, head) {
-  proxy.ws(req, socket, head, { target: 'ws://127.0.0.1:' + config.ports.tracker })
-}
-
 var httpServer = http.createServer(onRequest)
-httpServer.on('upgrade', onUpgrade)
 
 var httpsServer
 if (secretKey && secretCert) {
   httpsServer = https.createServer({ key: secretKey, cert: secretCert }, onRequest)
-  httpsServer.on('upgrade', onUpgrade)
 }
 
-var web, tracker
+var web
 
 auto({
   httpServer: function (cb) {
@@ -76,11 +64,7 @@ auto({
     if (httpsServer) httpsServer.listen(config.ports.router.https, config.host, cb)
     else cb(null)
   },
-  tracker: function (cb) {
-    tracker = spawn(path.join(__dirname, 'tracker'))
-    tracker.on('message', cb.bind(null, null))
-  },
-  downgrade: ['httpServer', 'httpsServer', 'tracker', function (results, cb) {
+  downgrade: ['httpServer', 'httpsServer', function (results, cb) {
     downgrade()
     cb(null)
   }],
