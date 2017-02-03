680e42b4e42c1318bd044214aec099561ae53f20
Feross Aboukhadijeh <feross@feross.org>
2017-02-02 16:54:01 -0800

    Document config.js; use port 4000 in development

--- config.js
+++ config.js
@@ -1,13 +1,37 @@
-var path = require('path')
+const path = require('path')
 
-var secret
-try { secret = require('./secret') } catch (err) {}
+const isProd = process.env.NODE_ENV === 'production'
 
+/**
+ * Is site running in production?
+ */
+exports.isProd = isProd
+
+/**
+ * WebTorrent Desktop version (used by auto-updater, landing page, etc.)
+ */
 exports.desktopVersion = '0.17.2'
 
-exports.isProd = process.env.NODE_ENV === 'production'
-exports.port = 9000
+/**
+ * Server listening port
+ */
+exports.port = isProd
+  ? 9000
+  : 4000
 
+/**
+ * Path to store log files
+ */
+exports.logPath = process.env.NODE_ENV === 'production'
+  ? '/home/feross/www/log/webtorrent.io'
+  : path.join(__dirname, 'logs')
+
+let secret
+try { secret = require('./secret') } catch (err) {}
+
+/**
+ * Gitter IRC bot credentials
+ */
 exports.gitterBot = {
   ircChannel: '#webtorrent',
   ircNick: 'irc-gitter-bot',
@@ -15,7 +39,3 @@ exports.gitterBot = {
   gitterRoom: 'feross/webtorrent',
   gitterApiKey: secret && secret.gitterApiKey
 }
-
-exports.logPath = process.env.NODE_ENV === 'production'
-  ? '/home/feross/www/log/webtorrent.io'
-  : path.join(__dirname, 'logs')
