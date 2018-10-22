0eafcc11516004297bc059ba91c8a1db4d94d9e5
Feross Aboukhadijeh <feross@feross.org>
2018-10-21 20:13:29 -0700

    ignore 400 and 416 errors

--- server/rollbar.js
+++ server/rollbar.js
@@ -8,9 +8,21 @@ if (isProd) {
     captureUncaught: true,
     captureUnhandledRejections: true,
     checkIgnore: (isUncaught, args) => {
-      // Ignore 404 errors
       const err = args[0]
-      return !isUncaught && err && (err.status === 404 || err.message === 'Range Not Satisfiable')
+
+      // Never ignore uncaught errors
+      if (isUncaught) return false
+
+      // Ignore 'Bad Request' errors
+      if (err.status === 400) return true
+
+      // Ignore 'Not Found' errors
+      if (err.status === 404) return true
+
+      // Ignore 'Range Not Satisfiable' errors
+      if (err.status === 416) return true
+
+      return false
     }
   })
 }
